e82b3da067e683d8393fb2405c75a488b7b4a751
Christopher Smith <cbsmith@cbsmith.org>
2014-05-08 13:21:05 +0000

    First commit

---
+++ App_Code/BlogPost.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Text;
+using System.Text.RegularExpressions;
+using Humanizer;
+using Lucene.Net.Linq;
+using System.Linq;
+
+/// <summary>
+/// Summary description for BlogPost
+/// </summary>
+public class BlogPost
+{
+	public string Url { get; set; }
+
+	public string Title { get; set; }
+
+	public DateTime PostedDate { get; set; }
+
+	public IEnumerable<string> Tags { get; set; }
+
+	public string Content { get; set; }
+
+	public string Summary { get; set; }
+
+    public BlogPost()
+	{
+	}
+	
+	public BlogPost(string filename, string input)
+    {
+		Url = "/Blog/" + filename;
+
+		//Parse out the metadata (if there is any)
+		Title = filename.Humanize(LetterCasing.Title);
+		PostedDate = DateTime.Now;
+		Tags = new List<string>();
+
+		int contentStart = 0;
+		foreach(Match match in Regex.Matches(input, "^(Title|Tags|Posted Date|Summary): (.*)$", RegexOptions.Multiline))
+		{
+			Console.WriteLine(match.Value);
+			string key = match.Groups[1].Value;
+			switch(key)
+			{
+				case "Title":
+					Title = match.Groups[2].Value;
+					break;
+				case "Summary":
+					Summary = match.Groups[2].Value;
+					break;
+				case "Posted Date":
+					DateTime postedDate;
+					DateTime.TryParse(match.Groups[2].Value, out postedDate);
+					PostedDate = postedDate;
+					break;
+				case "Tags":
+					Tags = match.Groups[2].Value.Split(',').ToList();
+					break;
+			}
+			contentStart = match.Index + match.Length;
+		}
+
+		Content = input.Substring(contentStart).TrimStart();
+    }
+}
---
+++ App_Code/LuceneProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lucene.Net.Linq;
+using Lucene.Net;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using Lucene.Net.Analysis.Standard;
+using System.IO;
+using System.Web.Hosting;
+
+/// <summary>
+/// Summary description for LuceneProvider
+/// </summary>
+public class LuceneProvider
+{
+	private static LuceneDataProvider _instance;
+	private static ReadOnlyLuceneDataProvider _readOnlyInstance;
+	private static DirectoryInfo _appDataFolder;
+
+	static LuceneProvider()
+	{
+		_appDataFolder = new DirectoryInfo(HostingEnvironment.MapPath("~/App_Data/BlogIndex"));
+		var directory = new SimpleFSDirectory(_appDataFolder);
+
+		IndexWriter writer;
+		FSDirectory indexFSDir = FSDirectory.Open(_appDataFolder, new Lucene.Net.Store.SimpleFSLockFactory(_appDataFolder));
+
+		try
+		{
+			writer = new IndexWriter(directory, new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30), IndexWriter.MaxFieldLength.UNLIMITED);
+		}
+
+		catch (LockObtainFailedException ex)
+		{
+			IndexWriter.Unlock(indexFSDir);
+			writer = new IndexWriter(directory, new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30), IndexWriter.MaxFieldLength.UNLIMITED);
+		}
+
+		_instance = new LuceneDataProvider(directory, writer.Analyzer, Lucene.Net.Util.Version.LUCENE_30, writer);
+		_readOnlyInstance = new ReadOnlyLuceneDataProvider(directory, Lucene.Net.Util.Version.LUCENE_30);
+	}
+	public static LuceneDataProvider Instance
+	{
+		get
+		{
+			return _instance;
+		}
+	}
+
+	public static ReadOnlyLuceneDataProvider ReadOnlyInstance
+	{
+		get
+		{
+			return _readOnlyInstance;
+		}
+	}
+	
+}
